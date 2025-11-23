c963e7afacd0b2cf3d9f367b1f53d7e44f71c769
Allan Mitanda <alan@globalwesttechnologies.org>
2025-11-23 14:00:30 +0300

    advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# LibraryProject/bookshelf/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm, BookSearchForm
+from .forms import ExampleForm
 
-# Create your views here.
+
+# Add a new book
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
+
+# Search books safely
+def search_books(request):
+    form = BookSearchForm(request.GET)
+    books = []
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/search_results.html', {'form': form, 'books': books})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    """
+    View to list all books.
+    Only accessible to users with 'can_view' permission.
+    """
+    books = Book.objects.all()  # Query all books
+    context = {
+        'books': books,
+        'example_form': ExampleForm()   # ← Optional: include ExampleForm if needed in template
+    }
+    return render(request, 'bookshelf/book_list.html', context)
